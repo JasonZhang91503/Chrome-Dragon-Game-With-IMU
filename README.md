a5f8c989c35584791629ae3aa5c9a73e8bd42b1e
Allen Chang <s91503486f@gmail.com.tw>
2019-05-14 04:32:35 +0800

    dinosaur game control

--- README.md
+++ README.md
@@ -1,22 +1,19 @@
 # Chrome Dragon Game With IMU
----
+
 ## What is IMU?
 IMU(Inertial measurement unit)包含加速度計、陀螺儀的晶片單元，用以算出物體的姿態。
 
----
 
 ## Google Dragon Game
 
 Google 離線狀態的情況下會出現一個可愛的恐龍遊戲，本專案利用嵌入式系統開發版來操控這隻可愛的小恐龍
 ![Dino Game](https://programcodelib.com/wp-content/uploads/2015/01/google-chrome-offline-game11.jpg)
 
----
 ## Pre-requirement
 * BeagleBone Black 開發版 x 1
 * LSM9DS0晶片(陀螺儀、加速度計、磁力計) x 1
 * Linux based os
 
----
 ## Installation
 
 * **Beaglebone Black**
--- control.py
+++ control.py
@@ -15,9 +15,21 @@ async def handler():
         await websocket.send("Works")
 
         while True:
-            data = await websocket.recv()
-            print(data)
+            ax = await websocket.recv()
+            ay = await websocket.recv()
+            az = await websocket.recv()
+            gx = await websocket.recv()
+            gy = await websocket.recv()
+            gz = await websocket.recv()
+            print("ax:", ax, " ay:", ay, " az:", az)
+            print("gx:", gx, " gy:", gy, " gz:", gz)
 
+            thehold = 80
+            if float(gx) > thehold or float(gy) > thehold or float(gz) > thehold:
+                pyautogui.press('space')  # Dinasour Jump!
+                await websocket.send("1")
+            else:
+                await websocket.send("0")
 
 
 asyncio.get_event_loop().run_until_complete(handler())
